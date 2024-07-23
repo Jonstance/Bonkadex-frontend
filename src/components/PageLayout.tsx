61b9e1bb8f637c79612ece5d859ef9d29bb8047a
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2024-07-23 20:10:46 +0100

    Update PageLayout.tsx

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -664,63 +664,20 @@ function SideMenu({ className, onClickCloseBtn }: { className?: string; onClickC
             >
               Liquidity
             </LinkItem>
-            <LinkItem
-              icon="/icons/entry-icon-concentrated-pools.svg"
-              href="/clmm/pools"
-              isCurrentRoutePath={pathname === '/clmm/pools'}
-            >
-              Concentrated
-            </LinkItem>
+            
             <LinkItem icon="/icons/entry-icon-pools.svg" href="/pools" isCurrentRoutePath={pathname === '/pools'}>
               Pools
             </LinkItem>
             <LinkItem icon="/icons/entry-icon-farms.svg" href="/farms" isCurrentRoutePath={pathname === '/farms'}>
               Farms
             </LinkItem>
-            <LinkItem icon="/icons/entry-icon-staking.svg" href="/staking" isCurrentRoutePath={pathname === '/staking'}>
-              Staking
-            </LinkItem>
-            <LinkItem icon="/icons/entry-icon-acceleraytor.svg" href="/acceleraytor/list">
-              AcceleRaytor
-            </LinkItem>
+            
+            
           </div>
 
-          <Col className="overflow-scroll no-native-scrollbar">
-            <div className="mx-8 border-b border-[rgba(57,208,216,0.16)] my-2 mobile:my-1"></div>
-            <div className="flex-1 overflow-auto no-native-scrollbar mt-2">
-              <RpcConnectionPanelSidebarWidget />
-              <SettingSidebarWidget />
-              <CommunityPanelSidebarWidget />
+         
 
-              <OptionItem noArrow href="https://raydium.gitbook.io/raydium/" iconSrc="/icons/msic-docs.svg">
-                Docs
-              </OptionItem>
-
-              <OptionItem noArrow href="https://v1.raydium.io/swap" heroIconName="desktop-computer">
-                Bonkadex V1
-              </OptionItem>
-
-              <OptionItem noArrow href="https://forms.gle/DvUS4YknduBgu2D7A" iconSrc="/icons/misc-feedback.svg">
-                Feedback
-              </OptionItem>
-            </div>
-          </Col>
-
-          <Tooltip>
-            <div className="text-sm mobile:text-xs m-2 mb-0 leading-relaxed opacity-50 hover:opacity-100 transition font-medium text-[#abc4ff] whitespace-nowrap cursor-default">
-              <div>V {currentVersion.slice(1)}</div>
-              <div>
-                <BlockTimeClock />
-              </div>
-            </div>
-            <Tooltip.Panel>
-              <div className="text-xs m-2 leading-relaxed font-medium text-[#abc4ff] whitespace-nowrap cursor-default">
-                <div>Current: {currentVersion}</div>
-                <div>Latest: {latestVersion}</div>
-                <div>Block time: {<BlockTimeClock showSeconds />}</div>
-              </div>
-            </Tooltip.Panel>
-          </Tooltip>
+       
         </Col>
       </Col>
     </>
@@ -760,12 +717,12 @@ function LinkItem({
       }`}
     >
       <Row className="items-center">
-        <div className="grid bg-gradient-to-br from-[rgba(57,208,216,0.2)] to-[rgba(57,208,216,0)] rounded-lg p-1.5 mr-3">
-          <Icon size={isMobile ? 'xs' : 'sm'} iconSrc={icon} />
+        <div className="grid bg-gradient-to-br  rounded-md p-1.5 mr-3">
+          <Icon size={isMobile ? 'xs' : 'sm'} iconSrc={icon}/>
         </div>
         <Row
-          className={`grow items-center justify-between text-[#ACE3E5] ${
-            isCurrentRoutePath ? 'text-[rgba(57,208,216,1)]' : ''
+          className={`grow items-center justify-between text-white ${
+            isCurrentRoutePath ? 'text-white' : ''
           } text-sm mobile:text-[0.85rem] font-medium`}
         >
           <div>{children}</div>

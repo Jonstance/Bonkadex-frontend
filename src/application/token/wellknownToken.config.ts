1abf53d21b01269d8f7718b5dad12ac13338accb
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2024-07-24 20:20:07 +0100

    Update wellknownToken.config.ts

--- src/application/token/wellknownToken.config.ts
+++ src/application/token/wellknownToken.config.ts
@@ -2,11 +2,11 @@ import toPubString, { toPub } from '@/functions/format/toMintString'
 import { PublicKey } from '@solana/web3.js'
 import { WSOLMint } from './quantumSOL'
 
-export const RAYMint = toPub('CQypd6aNEJpVhcKqmf5knLcGpfPAMtaBusz7vd25d9Ny')
+export const RAYMint = toPub('4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R')
 export const PAIMint = toPub('Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS')
 export const SRMMint = toPub('SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt')
 export const USDCMint = toPub('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v')
-export const USDTMint = toPub('CQypd6aNEJpVhcKqmf5knLcGpfPAMtaBusz7vd25d9Ny')
+export const USDTMint = toPub('Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB')
 export const mSOLMint = toPub('mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So')
 export const stSOLMint = toPub('7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj')
 export const USDHMint = toPub('USDH1SM1ojwWUga67PGrgFWUHibbjqMvuMaDkRJTgkX')
